9d992d444c7eebbae451aada8c4e4f9e074dfaca
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2022-07-18 23:20:30 +0545

    game finito

--- main.py
+++ main.py
@@ -1,4 +1,46 @@
-import pygame, sys 
+import pygame, sys, random
+
+def ball_animation():
+    global ball_speed_x, ball_speed_y
+    #animations
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= screen_height: 
+        ball_speed_y *= -1
+
+    if ball.left<=0 or ball.right >= screen_width:
+        ball_restart()
+
+    if ball.colliderect(player) or ball.colliderect(opponent): 
+        ball_speed_x *= -1
+
+def player_animation():
+    player.y += player_speed
+
+    if player.top <0: 
+        player.top = 0 #speed
+
+    if player.bottom >=screen_height:
+        player.bottom = screen_height
+
+def opponent_ai():
+    if opponent.top < ball.y:
+        opponent.top += opponent_speed 
+    if opponent.bottom > ball.y: 
+        opponent.bottom -= opponent_speed 
+
+    if opponent.top <0: 
+        opponent.top = 0 #speed
+
+    if opponent.bottom >=screen_height:
+        opponent.bottom = screen_height
+
+def ball_restart():
+    global ball_speed_x, ball_speed_y
+    ball.center = (screen_width/2, screen_height/2)
+    ball_speed_y *= random.choice((1,-1))
+    ball_speed_x *= random.choice((1,-1))
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -15,13 +57,35 @@ opponent = pygame.Rect(10, screen_height/2-70, 10, 140)
 bg_color = pygame.Color('grey12')
 light_grey = (200,200,200)
 
+ball_speed_x = 7 * random.choice((1,-1))
+ball_speed_y = 7 * random.choice((1,-1))
+
+player_speed = 0
+opponent_speed = 7
 
 while True: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             pygame.exit()
             sys.exit
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN: 
+                player_speed += 7 #speed
+            if event.key == pygame.K_UP: 
+                player_speed -= 7
 
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN: 
+                player_speed -= 7 #speed
+            if event.key == pygame.K_UP: 
+                player_speed += 7
+
+    ball_animation()
+    player_animation()
+    opponent_ai()
+
+    #visuals
     screen.fill(bg_color)
     pygame.draw.rect(screen, light_grey, player)
     pygame.draw.rect(screen, light_grey, opponent)
